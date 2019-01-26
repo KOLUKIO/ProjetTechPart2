146a9973de978dfe4f8c242b580d811cc529d2a4
manonlpz <manonlpz6@gmail.com>
2019-01-26 18:34:26 +0100

    initialisation

---
+++ camarchejamais/app/src/main/java/part2/tech/projet/u/edit/image/MainActivity.java
@@ -0,0 +1,13 @@
+package part2.tech.projet.u.edit.image;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
