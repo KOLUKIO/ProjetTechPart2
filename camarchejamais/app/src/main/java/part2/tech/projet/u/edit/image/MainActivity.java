46d50ee286304452495a7113525d6ca728361dec
manonlpz <manonlpz6@gmail.com>
2019-01-26 19:09:16 +0100

    new toolbar

--- camarchejamais/app/src/main/java/part2/tech/projet/u/edit/image/MainActivity.java
+++ camarchejamais/app/src/main/java/part2/tech/projet/u/edit/image/MainActivity.java
@@ -1,13 +1,67 @@
 package part2.tech.projet.u.edit.image;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SELECTED_PICTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMenu);
+        setSupportActionBar(toolbar);
+        // toolbar.setNavigationIcon(); // fleche retour
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.action_search_meme :
+                        // ...
+                    case R.id.action_picture_from_camera :
+                        // openCamera();
+                    case R.id.action_picture_from_gallery :
+                        // openGallery();
+                    default:
+                        return MainActivity.super.onOptionsItemSelected(menuItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void openCamera(){
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, 0);
+    }
+
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI); //, MediaStore.Images.Media.INTERNAL_CONTENT_URI
+        startActivityForResult(gallery, SELECTED_PICTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        // ...
     }
 }
