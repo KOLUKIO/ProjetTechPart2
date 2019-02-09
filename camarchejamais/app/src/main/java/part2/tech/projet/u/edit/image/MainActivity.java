eb6c47b275868032c82cad5e2c4221e6aabc8d4e
qduart <tinouduart33@gmail.com>
2019-02-09 19:00:08 +0100

    Take photo

--- camarchejamais/app/src/main/java/part2/tech/projet/u/edit/image/MainActivity.java
+++ camarchejamais/app/src/main/java/part2/tech/projet/u/edit/image/MainActivity.java
@@ -1,38 +1,58 @@
 package part2.tech.projet.u.edit.image;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int GALLERY_REQUEST = 1314;
+    private static final int REQUEST_TAKE_PHOTO = 1;
 
+    String photoPath;
+    private Uri photoUri;
     ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Ask permission WRITE_EXTERNAL_STORAGE */
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_TAKE_PHOTO);
+            }
+        } else {
+        }
+
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarMenu);
         setSupportActionBar(toolbar);
@@ -44,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onMenuItemClick(MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.action_picture_from_camera :
-                        // openCamera();
+                         openCamera();
                     case R.id.action_picture_from_gallery :
                          openGallery();
                     default:
@@ -61,8 +81,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openCamera(){
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camera, 0);
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photo = null;
+            try {
+                photo = createImageFile();
+            } catch (IOException ex)
+            { }
+            if (photo != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "part2.tech.projet.u.edit.image.fileprovider", photo);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + time + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+
+        photoPath = image.getAbsolutePath();
+        return image;
     }
 
     private void openGallery(){
@@ -77,9 +119,45 @@ public class MainActivity extends AppCompatActivity {
             Uri imageUri = data.getData();  // the address of the image on the SD Card
             imageView.setImageURI(imageUri);
 
-            File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
-            File file = new File(path, "app/src/main/res/drawable/demo.jpg");
+            Bitmap bmp = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
 
+            File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+            File file = new File(path, "demo.png");
+
+
+
+            try (FileOutputStream out = new FileOutputStream(file)) {
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+                // PNG is a lossless format, the compression factor (100) is ignored
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_TAKE_PHOTO) {
+            File photo = new File(photoPath);
+            try{
+                /* Get the photo */
+                Bitmap bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.fromFile(photo));
+                if (bmp != null)
+                {
+                    imageView.setImageBitmap(bmp);
+                }
+            }
+            catch (Exception e)
+            { }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_TAKE_PHOTO: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                }
+                return;
+            }
         }
     }
 }
