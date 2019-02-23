2bcb49387053d022f23502875d00b8ba1e7d3166
manonlpz <manonlpz6@gmail.com>
2019-02-23 20:56:30 +0100

    image ne cache plus la toolbar lors du zoom/scrol + ne peut pas etre deplace lorsque la taille de l'image est plus petite que la taille d'origine de l'image

--- EditImage/app/src/main/java/project/part2/editimage/Gesture.java
+++ EditImage/app/src/main/java/project/part2/editimage/Gesture.java
@@ -5,6 +5,7 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 public class Gesture extends GestureDetector.SimpleOnGestureListener {
+
     @Override
     public boolean onDown(MotionEvent event) {
         Log.d("TAG","onDown: ");
@@ -32,42 +33,41 @@ public class Gesture extends GestureDetector.SimpleOnGestureListener {
     }
 
     @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2,
-                            float distanceX, float distanceY) {
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
         Log.i("TAG", "onScroll: ");
-        String s = Integer.toString(e1.getPointerCount());
 
-        //System.out.println("fingers : " + e1.getPointerCount());
-
-        if (e2.getPointerCount() > 1)
-        {
-            if (distanceX > 0 || distanceY > 0)
-            {
+        if (e2.getPointerCount() > 1) {  // two or more fingers
+            // allows to enlarge the image
+            if (distanceX > 0 || distanceY > 0) {
                 MainActivity.imageView.setScaleX(MainActivity.imageView.getScaleX() + 0.1f);
                 MainActivity.imageView.setScaleY(MainActivity.imageView.getScaleY() + 0.1f);
             }
-
-            if (distanceX < 0 || distanceY < 0)
-            {
+            // allows to shrink the image
+            if (distanceX < 0 || distanceY < 0) {
                 MainActivity.imageView.setScaleX(MainActivity.imageView.getScaleX() - 0.1f);
                 MainActivity.imageView.setScaleY(MainActivity.imageView.getScaleY() - 0.1f);
             }
-
-            if (MainActivity.imageView.getScaleX() < 0.5)
-            {
-                MainActivity.imageView.setScaleX(0.5f);
-                MainActivity.imageView.setScaleY(0.5f);
+            // allows not to shrink the image more than 0.3 times its size
+            if (MainActivity.imageView.getScaleX() < 0.3) {
+                MainActivity.imageView.setScaleX(0.3f);
+                MainActivity.imageView.setScaleY(0.3f);
+            }
+            // allows not to enlarge the image more than 2.5 times its size
+            if (MainActivity.imageView.getScaleX() > 2.5) {
+                MainActivity.imageView.setScaleX(2.5f);
+                MainActivity.imageView.setScaleY(2.5f);
             }
         }
-        else{
-            MainActivity.imageView.scrollBy((int)distanceX, (int)distanceY);
+        else{                                            // one fingers, allow to move the image
+            if(MainActivity.imageView.getScaleX() > 1){  // if the size of the image is larger than the original size of the image
+                MainActivity.imageView.scrollBy((int)distanceX, (int)distanceY);
+            }
         }
         return true;
     }
 
     @Override
-    public boolean onFling(MotionEvent event1, MotionEvent event2,
-                           float velocityX, float velocityY) {
+    public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
         Log.d("TAG", "onFling: ");
 
         return true;
