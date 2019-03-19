e218e385449eda4cae4f3b3f5c4a2105c5158cd8
qduart <tinouduart33@gmail.com>
2019-03-19 18:00:24 +0100

    Blur rs (ScriptIntrinsicBlur)

--- EditImage/app/src/main/java/project/part2/editimage/Convolution.java
+++ EditImage/app/src/main/java/project/part2/editimage/Convolution.java
@@ -2,6 +2,11 @@ package project.part2.editimage;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.support.v8.renderscript.Allocation;
+import android.support.v8.renderscript.Element;
+import android.support.v8.renderscript.RenderScript;
+import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import android.support.v8.renderscript.Type;
 import android.util.Log;
 
 import static project.part2.editimage.Filters.*;
@@ -170,5 +175,40 @@ public class Convolution {
             res[j] = Color.rgb(ng, ng, ng);
         }
         bmp.setPixels(res, 0, width,0,0, width, height);
+    }
+
+    public static void BlurRS(Bitmap bmp, int radius)
+    {
+        //Create renderscript
+        RenderScript rs = RenderScript.create(MainActivity.getContext());
+
+        //Create allocation from Bitmap
+        Allocation allocation = Allocation.createFromBitmap(rs, bmp);
+
+        Type t = allocation.getType();
+
+        //Create allocation with the same type
+        Allocation blurredAllocation = Allocation.createTyped(rs, t);
+
+        //Create script
+        ScriptIntrinsicBlur blurScript = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+        //Set blur radius (maximum 25.0)
+        blurScript.setRadius(radius);
+        //Set input for script
+        blurScript.setInput(allocation);
+        //Call script for output allocation
+        blurScript.forEach(blurredAllocation);
+
+        //Copy script result into bitmap
+        blurredAllocation.copyTo(bmp);
+
+        //Destroy everything to free memory
+        allocation.destroy();
+        blurredAllocation.copyTo(bmp);
+        blurredAllocation.destroy();
+        blurScript.destroy();
+        t.destroy();
+        rs.destroy();
+        //return bmp;
     }
 }
--- EditImage/app/src/main/java/project/part2/editimage/ConvolutionFragment.java
+++ EditImage/app/src/main/java/project/part2/editimage/ConvolutionFragment.java
@@ -1,13 +1,19 @@
 package project.part2.editimage;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.renderscript.ScriptGroup;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import static project.part2.editimage.Convolution.*;
@@ -16,7 +22,8 @@ public class ConvolutionFragment extends Fragment {
 
     Bitmap bitmap;
     ImageView i;
-    private Button mButtonAveraging3, mButtonAveraging5, mButtonGaus5, mButtonSobel;
+    private Button mButtonAveraging3, mButtonAveraging5, mButtonGaus5, mButtonSobel, mButtonBlurRs;
+    int radius = 0;
 
     public ConvolutionFragment() {
         // Required empty public constructor
@@ -36,6 +43,7 @@ public class ConvolutionFragment extends Fragment {
         mButtonAveraging5 = view.findViewById(R.id.button_convolution_averaging_5);
         mButtonGaus5 = (Button) view.findViewById(R.id.button_convolution_gauss);
         mButtonSobel = (Button) view.findViewById(R.id.button_convolution_sobel);
+        mButtonBlurRs = (Button) view.findViewById(R.id.button_blurRs);
 
         mButtonAveraging3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,7 +77,40 @@ public class ConvolutionFragment extends Fragment {
             }
         });
 
+        mButtonBlurRs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Alert();
+            }
+        });
+
         return view;
     }
+    public void Alert()
+    {
+        AlertDialog.Builder radiusDialog = new AlertDialog.Builder(getActivity());
+        radiusDialog.setTitle("Set radius");
 
+        final EditText radiusText = new EditText(MainActivity.getContext());
+
+        radiusText.setInputType(InputType.TYPE_CLASS_NUMBER);
+        radiusDialog.setView(radiusText);
+
+        radiusDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                radius = Integer.parseInt(radiusText.getText().toString());
+                BlurRS(bitmap, radius);
+                i.setImageBitmap(bitmap);
+            }
+        });
+        radiusDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        radiusDialog.show();
+    }
 }
